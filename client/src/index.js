ffb79e9bad991dbf9e83f6d5ed9fdd909c18f265
jsdrever <jsdrever09@gmail.com>
2023-05-08 13:39:52 -0500

    tryna fix a route

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+//! what the hell is going on here?
 
 ReactDOM.render(
   <React.StrictMode>
